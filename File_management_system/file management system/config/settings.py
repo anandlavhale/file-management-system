572dd738838d689db1714b7c064a2595cdfaa42d
anandlavhale <aml862005@gmail.com>
2025-12-08 19:00:26 +0530

    1
    hjhf

--- File_management_system/file management system/config/settings.py
+++ File_management_system/file management system/config/settings.py
@@ -100,10 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Don't use STATICFILES_DIRS when static files are in app directories
-# Django will automatically find them in records/static/
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'records', 'static')]
 
 # WhiteNoise configuration for serving static files in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
